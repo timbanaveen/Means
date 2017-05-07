ed08377c07fac0b2569f5658a15b1c208454c0cb
timbanaveen <timbanaveen@gmail.com>
2017-05-07 16:06:48 +0530

    new route for fetching article

--- api/models/articles.js
+++ api/models/articles.js
@@ -1,11 +1,33 @@
-var collectionKey = 'articles';
+const ObjectID = require('mongodb').ObjectID;
 
-var model = {
+const articlesCollectionKey = 'articles';
+
+/**
+ * Object which holds all methods to fetch data.
+ * 
+ * quering collection in each method, 
+ * as it may happen that some data is stored in different collection of same db
+ */
+const model = {
     getAllArticles: function(db) {
         return new Promise((resolve, reject) => {
-            const collection = db.collection(collectionKey);
+            const collection = db.collection(articlesCollectionKey);
 
             collection.find({}).toArray((err, docs) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(docs);
+                }
+            });
+        });
+    },
+
+    getArticle: function(db, id) {
+        return new Promise((resolve, reject) => {
+            const collection = db.collection(articlesCollectionKey);
+
+            collection.find({ _id: ObjectID(id) }).toArray((err, docs) => {
                 if (err) {
                     reject(err);
                 } else {
--- api/routes.js
+++ api/routes.js
@@ -15,6 +15,22 @@ function initRoutes(app) {
                 res.status(400).send(err.message);
             });
      });
+
+     /**
+      * Get article having id equal to passed id.
+      */
+     app.get('/article/:articleId', function(req, res) {
+        const articleId = req.params.articleId;
+
+        articles.getArticle(app.locals.db, articleId)
+            .then((article) => {
+                res.json(article);
+            })
+            .catch((err) => {
+                console.error(err.stack);
+                res.status(400).send(err.message);
+            }); // doing same thing in catch, should move to middleware.
+     });
 }
 
 module.exports = initRoutes;
